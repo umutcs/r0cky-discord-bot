8653eb0e463b02e13bcdb44a380b5b80fac002e4
Umut <umtyrdql@gmail.com>
2023-04-23 22:29:16 +0300

    commit to maİN
    Co-Authored-By: caps <61022598+capsaicin01@users.noreply.github.com>

--- ConsoleApp1/ConsoleApp1/Bot.cs
+++ ConsoleApp1/ConsoleApp1/Bot.cs
@@ -11,23 +11,18 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 
-
-
 namespace r0cky
 {
-
     public class Bot
     {
-
         public DiscordClient DiscordClient { get; private set; }
         public InteractivityExtension Interactivity { get; private set; }
         public CommandsNextExtension Commands { get; private set; }
+
         public async Task RunAsync()
         {
             var json = string.Empty;
@@ -38,50 +33,40 @@ namespace r0cky
             var configjson = JsonConvert.DeserializeObject<ConfigJson>(json);
             var config = new DiscordConfiguration
             {
-                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
                 Token = configjson.Token,
                 TokenType = TokenType.Bot,
                 MinimumLogLevel = LogLevel.Debug
-
             };
 
             DiscordClient = new DiscordClient(config);
-            DiscordClient.UseInteractivity(new InteractivityConfiguration()
+            Interactivity = DiscordClient.UseInteractivity(new InteractivityConfiguration()
             {
                 Timeout = TimeSpan.FromMinutes(2)
             });
-            var commandsconfig = new CommandsNextConfiguration()
+            Commands = DiscordClient.UseCommandsNext(new CommandsNextConfiguration
             {
                 StringPrefixes = new string[] { configjson.Prefix },
-                EnableMentionPrefix = true,
                 EnableDms = true,
-                EnableDefaultHelp = false
-            };
-            Commands = DiscordClient.UseCommandsNext(commandsconfig);
+                EnableMentionPrefix = true,
+                DmHelp = true,
+                IgnoreExtraArguments = true,
+                CaseSensitive = false,
+                Services = null
+            });
+
             Commands.RegisterCommands<FunCommands>();
             Commands.RegisterCommands<ModerationCommands>();
             Commands.RegisterCommands<Blackjack>();
 
             DiscordClient.Ready += OnClientReady;
-
-            //DiscordClient.MessageCreated += OnMessageCreated;
-
             await DiscordClient.ConnectAsync();
             await Task.Delay(-1);
-
-
-            //-1 dememmizin sebebi eğer ki connection atarken hata olursa bunu engellemesi ve tekrardan denemesinde yardımcı olması
-            //içindir.
-
         }
 
         private Task OnClientReady(DiscordClient sender, ReadyEventArgs e)
         {
-            Console.WriteLine("Bot is ready!");
-
+            Console.WriteLine($"Logged in as {sender.CurrentUser.Username}#{sender.CurrentUser.Discriminator}");
             return Task.CompletedTask;
-
         }
-
     }
 }
